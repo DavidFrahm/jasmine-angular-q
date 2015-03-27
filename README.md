9aa287ed65dfff72e37ea0fd2971189b5d96ac86
David Frahm <david@midmoapps.com>
2015-03-27 10:33:46 -0500

    Adds library from my personal projects

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # jasming-angular-q
+
 Helper functions for dealing with AngularJS promises in Jasmine specs
---
+++ jasmine-angular-q.js
@@ -0,0 +1,85 @@
+/*
+ * Copyright © 2013 David Frahm, @DavidFrahm,
+ * https://github.com/DavidFrahm
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation files
+ * (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+var jasmineNgQ = {
+
+    q: null,
+
+    useQ: function(q) {
+       this.q = q
+    },
+
+    spyOnAndResolve: function(source, target, retval, q) {
+       q = q || this.q;
+
+       return spyOn(source, target).andCallFake(function() {
+           var deferred = q.defer();
+           deferred.resolve(retval);
+           return deferred.promise;
+       });
+    },
+
+    spyOnAndReject: function(source, target, retval, q) {
+       q = q || this.q;
+
+       return spyOn(source, target).andCallFake(function() {
+           var deferred = q.defer();
+           deferred.reject(retval);
+           return deferred.promise;
+       });
+    },
+
+    createPromiseResolvingSpy: function(q, retval) {
+        return jasmine.createSpy().andCallFake(function() {
+            var deferred = q.defer();
+            deferred.resolve(retval);
+            return deferred.promise;
+        });
+    },
+
+    createPromiseRejectingSpy: function(q, retval) {
+        return jasmine.createSpy().andCallFake(function() {
+            var deferred = q.defer();
+            deferred.reject(retval);
+            return deferred.promise;
+        });
+    },
+
+    createResourcePromiseResolvingSpy: function(q, retval) {
+        return jasmine.createSpy().andCallFake(function() {
+            var deferred = q.defer();
+            deferred.resolve(retval);
+            return {$promise: deferred.promise};
+        });
+    },
+
+    createResourcePromiseRejectingSpy: function(q, retval) {
+        return jasmine.createSpy().andCallFake(function() {
+            var deferred = q.defer();
+            deferred.reject(retval);
+            return {$promise: deferred.promise};
+        });
+    }
+};
